cab9ed8df4249a7af8e9c9be7af3c685dea5da45
shiro <shiro.mns@gmail.com>
2018-03-11 02:51:04 +0900

    [追加]多重購入一覧ページに販売数、トップページ、上部ナビからのリンク

--- app/controllers/multiple_buyings_controller.rb
+++ app/controllers/multiple_buyings_controller.rb
@@ -7,7 +7,8 @@ class MultipleBuyingsController < ApplicationController
     param_set
     @count	= MultipleBuying.includes([:p_name, :fortress_data]).group([:result_no, :e_no]).search(params[:q]).result.count().keys().size()
     @search	= MultipleBuying.includes([:p_name, :fortress_data]).group([:result_no, :e_no])
-        .select("*, max(multiple_buyings.multiple_buying) AS max, min(multiple_buyings.multiple_buying) AS min")
+        .select("*, max(multiple_buyings.multiple_buying) AS multiple_buying_max, min(multiple_buyings.multiple_buying) AS multiple_buying_min,
+                    max(multiple_buyings.buy_num) AS buy_num_max, min(multiple_buyings.buy_num) AS buy_num_min")
         .page(params[:page]).search(params[:q])
 
     @search.sorts = 'id asc' if @search.sorts.empty?
@@ -24,6 +25,7 @@ class MultipleBuyingsController < ApplicationController
     reference_text_assign(params, "p_name_name", "p_name_form")
     reference_number_assign(params, "battle_no", "battle_no_form")
     reference_number_assign(params, "multiple_buying", "multiple_buying_form")
+    reference_number_assign(params, "buy_num", "buy_num_form")
     reference_number_assign(params, "fortress_data_grand", "grand_form")
         
     @result_no_form = params["result_no_form"]
@@ -31,6 +33,7 @@ class MultipleBuyingsController < ApplicationController
     @e_no_form = params["e_no_form"]
     @battle_no_form = params["battle_no_form"]
     @multiple_buying_form = params["multiple_buying_form"]
+    @buy_num_form = params["buy_num_form"]
     @grand_form = params["grand_form"]
   end
   # GET /multiple_buyings/1
